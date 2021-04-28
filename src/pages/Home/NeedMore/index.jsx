3600e1b530fc6f56626e5238e629e42ad074a6a9
= <=>
2021-04-29 05:22:57 +0700

    Done NeedMore

---
+++ src/pages/Home/NeedMore/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Row, Col } from 'antd';
+import './styles.scss'
+
+
+
+function NeedMore(props) {
+    return (
+        <div className="container">
+            <div className="needmore">
+                <Row className="needmore-content">
+                    <Col span={8}>
+                        <h3>Need More Information On Real Estate?</h3>
+                    </Col>
+                    <Col span={16}>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis quibusdam consectetur, nulla tempore quas iste, aliquid esse animi, delectus sed libero ad aspernatur magni hic nemo modi blanditiis quod corporis!</p>
+                        <a className="needmore-content-btn" href="#">Find Out More</a>
+                    </Col>
+                </Row>
+
+            </div>
+        </div>
+    );
+}
+
+export default NeedMore;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import Banner from './Banner';
 import Category from './Category';
 import Testimonials from './Testimonials';
+import NeedMore from './NeedMore';
 
 function Home(props) {
     return (
         <div>
             {/* <Banner /> */}
             {/* <Category /> */}
-            <Testimonials />
+            {/* <Testimonials /> */}
+            <NeedMore />
         </div>
     );
 }
