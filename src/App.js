9e066e2594861c970fffe5e9b876dc22a62dcd63
= <=>
2021-04-29 01:56:39 +0700

    Done Banner

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./pages/Home";
 function App() {
     return (
         <div className="app">
-            <Header />
+            {/* <Header /> */}
             <Home />
             {/* <Footer /> */}
         </div>
