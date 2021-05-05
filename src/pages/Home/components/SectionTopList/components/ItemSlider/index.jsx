46fc39b2deb34c047aeb839fda27441f8b3db0f0
Vu Khanh <vukhanh.nguyen@citynow.vn>
2021-05-05 12:21:25 +0700

    Complete homepage

--- src/pages/Home/components/SectionTopList/components/ItemSlider/index.jsx
+++ src/pages/Home/components/SectionTopList/components/ItemSlider/index.jsx
@@ -17,50 +17,52 @@ function ItemSlider() {
     return (
         <div className="item-slider" style={{ backgroundImage: "URL('http://androthemes.com/themes/react/acres/assets/img/listings/3.jpg')" }}>
             <div className="item-wrapper">
-                <div className="listing-primary">
-                    <div className="listing__body">
-                        <h5 class="listing-title">
-                            <a title="Iris Watson, Frederick Nebraska 20620">
-                                Iris Watson, Frederick Nebraska 20620
+                <div className="item">
+                    <div className="listing-primary">
+                        <div className="listing__body">
+                            <h5 class="listing-title">
+                                <a title="Iris Watson, Frederick Nebraska 20620">
+                                    Iris Watson, Frederick Nebraska 20620
                     </a>
-                        </h5>
-                        <div className="author">
-                            <a>
-                                <img src="http://androthemes.com/themes/react/acres/assets/img/listing-single/6.jpg" alt="agent" />
-                            </a>
-                            <div className="media-body">
-                                <h6>
-                                    Freddy Burben
+                            </h5>
+                            <div className="author">
+                                <a>
+                                    <img src="http://androthemes.com/themes/react/acres/assets/img/listing-single/6.jpg" alt="agent" />
+                                </a>
+                                <div className="media-body">
+                                    <h6>
+                                        Freddy Burben
                     </h6>
-                                <span>March 3, 2020</span>
+                                    <span>March 3, 2020</span>
+                                </div>
+                                <div className="icon-wrapper">
+                                    <Popover className="popup" content={content} trigger="click">
+                                        <div className="icon">
+                                            <i className="fas fa-ellipsis-v">
+                                            </i>
+                                        </div>
+                                    </Popover>
+                                </div>
                             </div>
-                            <div className="icon-wrapper">
-                                <Popover className="popup" content={content} trigger="click">
-                                    <div className="icon">
-                                        <i className="fas fa-ellipsis-v">
-                                        </i>
-                                    </div>
-                                </Popover>
+                            <span class="listing-price">3,500$</span>
+                            <p class="listing-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                            <div class="acr-listing-icons">
+                                <div class="acr-listing-icon">
+                                    <i class="flaticon-pillow"></i>
+                                    <span class="acr-listing-icon-value">3</span>
+                                </div>
+                                <div class="acr-listing-icon">
+                                    <i class="flaticon-bathtub"></i>
+                                    <span class="acr-listing-icon-value">2</span>
+                                </div>
+                                <div class="acr-listing-icon">
+                                    <i class="flaticon-ruler"></i>
+                                    <span class="acr-listing-icon-value">2,499</span>
+                                </div>
                             </div>
-                        </div>
-                        <span class="listing-price">3,500$</span>
-                        <p class="listing-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                        <div class="acr-listing-icons">
-                            <div class="acr-listing-icon">
-                                <i class="flaticon-pillow"></i>
-                                <span class="acr-listing-icon-value">3</span>
+                            <div class="listing-gallery-wrapper">
+                                <Button value="View Details" className="view-detail"></Button>
                             </div>
-                            <div class="acr-listing-icon">
-                                <i class="flaticon-bathtub"></i>
-                                <span class="acr-listing-icon-value">2</span>
-                            </div>
-                            <div class="acr-listing-icon">
-                                <i class="flaticon-ruler"></i>
-                                <span class="acr-listing-icon-value">2,499</span>
-                            </div>
-                        </div>
-                        <div class="listing-gallery-wrapper">
-                            <Button value="View Details" className="view-detail"></Button>
                         </div>
                     </div>
                 </div>
--- src/pages/Home/components/SectionTopList/index.jsx
+++ src/pages/Home/components/SectionTopList/index.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import { Col, Row } from 'antd';
-import './styles.scss';
+import { Carousel } from 'antd';
+import React, { useState, useRef } from 'react';
 import ItemSlider from './components/ItemSlider';
+import './styles.scss';
 
 function SectionTopList() {
+    const [slider, setSlider] = useState()
+    const carousel = useRef(null);
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 800,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
     return (
         <div className="section-top-list">
             <div className="container">
@@ -12,7 +22,14 @@ function SectionTopList() {
                     <h2 class="title">Our Top Listing</h2>
                 </div>
                 <div className="slider">
-                    <ItemSlider />
+                    <div className="arrows">
+                        <i className="slider-prev fas fa-arrow-left slick-arrow" onClick={() => { carousel.current.prev(); }} ></i>
+                        <i className="slider-next fas fa-arrow-right slick-arrow" onClick={() => { carousel.current.next(); }} ></i>
+                    </div>
+                    <Carousel ref={carousel} {...settings} >
+                        <ItemSlider />
+                        <ItemSlider />
+                    </Carousel>
                 </div>
             </div>
         </div >
